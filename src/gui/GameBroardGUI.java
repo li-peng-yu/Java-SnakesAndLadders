835d644e3d5bee47080c416faefc11d9343b32ee
missionary_Rand <143264937+li-peng-yu@users.noreply.github.com>
2025-04-22 21:15:11 +0800

    Update GameBroardGUI.java

--- src/gui/GameBroardGUI.java
+++ src/gui/GameBroardGUI.java
@@ -1,1 +1,14 @@
+package gui;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+public class GameBoardGUI {
+
+    public static Scene getGameScene() {
+        BorderPane root = new BorderPane();
+        root.setStyle("-fx-background-color: #f0f0f0;");
+        // 在这里添加你的棋盘、按钮等 UI 元素
+        return new Scene(root, 800, 600);
+    }
+}
