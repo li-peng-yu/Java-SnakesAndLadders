3610548730a79c3d63cf8d99fbdd1bf2f48cf684
missionary_Rand <143264937+li-peng-yu@users.noreply.github.com>
2025-04-22 21:14:43 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,23 @@
+import gui.LogoScene;
+import gui.GameBoardGUI;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Snakes and Ladders");
+
+        // 显示Logo页，3秒后加载游戏主界面
+        primaryStage.setScene(LogoScene.getLogoScene(() -> {
+            primaryStage.setScene(GameBoardGUI.getGameScene());
+        }));
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
