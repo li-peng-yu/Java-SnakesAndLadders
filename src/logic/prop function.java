b7b05aa4432f6e8a377e58bf342290983fc3b708
zhong12350 <2324811869@qq.com>
2025-05-09 17:24:26 +0800

    Update prop function.java

--- src/logic/prop function.java
+++ src/logic/prop function.java
@@ -1,1 +1,20 @@
-
+public enum Item {
+    FROZEN_TRAP("冰冻陷阱", "冻结对手一回合"),
+    POSITION_SWAP("位置交换器", "立即与对手交换位置");
+    
+    private final String name;
+    private final String description;
+    
+    Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+}
